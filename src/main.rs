ab8482244d50826202eb4c2405c407df58dcda5c
wesleynepo <wesleynepomuceno1@gmail.com>
2022-04-05 22:40:14 -0300

    feat: initial state

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/api/check/{cep}")]
+async fn check(_cep: web::Path<String>) -> impl Responder {
+
+    HttpResponse::Ok().body("Bogdan")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(check))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
